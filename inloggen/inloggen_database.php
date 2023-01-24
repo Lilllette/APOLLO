cc32410761f8a2deb0f26cd5d892fd859b7f9a00
Lilllette <105648265+Lilllette@users.noreply.github.com>
2023-01-24 08:30:09 +0100

    Update inloggen_database.php

--- inloggen/inloggen_database.php
+++ inloggen/inloggen_database.php
@@ -1,1 +1,20 @@
+<?php
 
+$host = "192.168.56.104";
+$dbname = "inloggen";
+$username = "root";
+$password = "";
+
+$mysqli = new mysqli(hostname: $host,
+                     username: $username,
+                     password: $password,
+                     database: $dbname);
+
+                     
+if ($mysqli->connect_errno) {
+    die("Connectie fout: " . $mysqli->connect_error);
+}
+
+return $mysqli;
+
+?>
