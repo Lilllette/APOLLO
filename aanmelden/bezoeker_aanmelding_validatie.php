d0e6b757859e112c9268c7490fc11fae2965e987
Lilllette <105648265+Lilllette@users.noreply.github.com>
2023-01-24 08:31:12 +0100

    Update bezoeker_aanmelding_validatie.php

--- aanmelden/bezoeker_aanmelding_validatie.php
+++ aanmelden/bezoeker_aanmelding_validatie.php
@@ -1,1 +1,23 @@
+<html lang="en">
+<head>
+	<title>Bezoeker aanmelding validatie</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width-device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+</head>
 
+<?php
+
+if (empty($_POST["naam"])) {
+	die("Naam is nodig");
+}
+
+if ( empty($_POST["email"])) {
+	die("E-mail adress is nodig");
+} 
+
+print_r($_POST);
+
+header("Location: succesvolle_aanmelding.html");
+
+?>
