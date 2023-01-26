7368eb019b87f0bf05d6f4c6db1da6b9c87dbc40
Lilllette <105648265+Lilllette@users.noreply.github.com>
2023-01-26 12:49:55 +0100

    Create enquete_validatie.php

---
+++ enquete/enquete_validatie.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+	<title>Bezoeker aanmelding validatie</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width-device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+</head>
+
+<?php
+
+print_r($_POST);
+
+header("Location: succesvol_verzonden.html");
+
+?>
